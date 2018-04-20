dad4b498e95645fb969ebf378bccb198a2b29b55
Remington Campbell <remcampb@cisco.com>
2018-04-19 18:07:58 -0700

    Use cache in toolchain

--- script/toolchain.sh
+++ script/toolchain.sh
@@ -2,9 +2,16 @@
 echo "Downloading toolchain..."
 pushd /build/
 mkdir toolchain
-cd toolchain
-wget --no-verbose https://releases.linaro.org/components/toolchain/binaries/5.2-2015.11-2/aarch64-linux-gnu/gcc-linaro-5.2-2015.11-2-x86_64_aarch64-linux-gnu.tar.xz
+TOOLCHAIN_FILENAME=gcc-linaro-5.2-2015.11-2-x86_64_aarch64-linux-gnu.tar.xz
+FQ_TOOLCHAIN_FILENAME=$CACHE_DIR$TOOLCHAIN_FILENAME
+if [ -f "${FQ_TOOLCHAIN_FILENAME}" ]; then
+    cp $FQ_TOOLCHAIN_FILENAME .
+else
+    wget --no-verbose https://releases.linaro.org/components/toolchain/binaries/5.2-2015.11-2/aarch64-linux-gnu/gcc-linaro-5.2-2015.11-2-x86_64_aarch64-linux-gnu.tar.xz
+    cp $TOOLCHAIN_FILENAME $FQ_TOOLCHAIN_FILENAME
+fi
 tar --extract --xz --file=gcc-linaro-5.2-2015.11-2-x86_64_aarch64-linux-gnu.tar.xz
+rm $TOOLCHAIN_FILENAME
 sync
 export PATH=$PATH:/build/toolchain/gcc-linaro-5.2-2015.11-2-x86_64_aarch64-linux-gnu/bin
 echo "Finished extracting toolchain."
