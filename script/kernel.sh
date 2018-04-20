e366951eedf47dd88420dae1fcba4f72c0ad09e9
Remington Campbell <remcampb@cisco.com>
2018-04-19 18:14:59 -0700

    Use cache in kernel build

--- script/kernel.sh
+++ script/kernel.sh
@@ -4,9 +4,16 @@ pushd /build/
 mkdir -p kernel/4.4.8
 cd kernel/4.4.8
 mkdir -p /build/cache/
-wget https://github.com/MarvellEmbeddedProcessors/linux-marvell/archive/linux-4.4.8-armada-17.02-espressobin.zip
-unzip linux-4.4.8-armada-17.02-espressobin.zip
-mv linux-4.4.8-armada-17.02-espressobin.zip /build/cache/
+KERNEL_FILENAME=linux-4.4.8-armada-17.02-espressobin.zip
+FQ_KERNEL_FILENAME=$CACHE_DIR$KERNEL_FILENAME
+if [ -f "${FQ_KERNEL_FILENAME}" ]; then
+    cp $FQ_KERNEL_FILENAME $KERNEL_FILENAME
+else
+    wget --no-verbose https://github.com/MarvellEmbeddedProcessors/linux-marvell/archive/$KERNEL_FILENAME
+    cp $KERNEL_FILENAME FQ_KERNEL_FILENAME
+fi
+unzip $KERNEL_FILENAME
+rm $KERNEL_FILENAME
 sync
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
