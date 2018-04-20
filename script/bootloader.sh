0e185041fc8d71c9aed6b81c83f4a3f25f084caf
Remington Campbell <remcampb@cisco.com>
2018-04-19 18:28:33 -0700

    Use cache in bootloader build

--- script/bootloader.sh
+++ script/bootloader.sh
@@ -4,9 +4,16 @@ echo "Downloading u-boot..."
 mkdir u-boot
 cd u-boot
 mkdir -p /build/cache/
-wget https://github.com/MarvellEmbeddedProcessors/u-boot-marvell/archive/u-boot-2017.03-armada-17.10.zip
-unzip u-boot-2017.03-armada-17.10.zip
-mv u-boot-2017.03-armada-17.10.zip /build/cache/
+UBOOT_FILENAME=u-boot-2017.03-armada-17.10.zip
+FQ_UBOOT_FILENAME=$CACHE_DIR$UBOOT_FILENAME
+if [ -f "${FQ_UBOOT_FILENAME}" ]; then
+    cp $FQ_UBOOT_FILENAME $UBOOT_FILENAME
+else
+    wget --no-verbose https://github.com/MarvellEmbeddedProcessors/u-boot-marvell/archive/$UBOOT_FILENAME
+    cp $UBOOT_FILENAME $FQ_UBOOT_FILENAME
+fi
+unzip $UBOOT_FILENAME
+rm $UBOOT_FILENAME
 sync
 echo "Building u-boot..."
 make mvebu_espressobin-88f3720_defconfig
@@ -21,16 +28,30 @@ cd ..
 echo "Downloading atf..."
 mkdir atf
 cd atf
-wget https://github.com/MarvellEmbeddedProcessors/atf-marvell/archive/atf-v1.3-armada-17.10.zip
-unzip atf-v1.3-armada-17.10.zip
-mv atf-v1.3-armada-17.10.zip /build/cache/
+ATF_FILENAME=atf-v1.3-armada-17.10.zip
+FQ_ATF_FILENAME=$CACHE_DIR$ATF_FILENAME
+if [ -f "${FQ_ATF_FILENAME}" ]; then
+    cp $FQ_ATF_FILENAME $ATF_FILENAME
+else
+    wget --no-verbose https://github.com/MarvellEmbeddedProcessors/atf-marvell/archive/$ATF_FILENAME
+    cp $ATF_FILENAME $FQ_ATF_FILENAME
+fi
+unzip $ATF_FILENAME
+rm $ATF_FILENAME
 sync
 cd ..
 mkdir a3700-utils
 cd a3700-utils
-wget https://github.com/MarvellEmbeddedProcessors/A3700-utils-marvell/archive/A3700_utils-armada-17.10.zip
-unzip A3700_utils-armada-17.10.zip
-mv A3700_utils-armada-17.10.zip /build/cache/
+UTILS_FILENAME=A3700_utils-armada-17.10.zip
+FQ_UTILS_FILENAME=$CACHE_DIR$UTILS_FILENAME
+if [ -f "${FQ_UTILS_FILENAME}" ]; then
+    cp $FQ_UTILS_FILENAME $UTILS_FILENAME
+else
+    wget --no-verbose https://github.com/MarvellEmbeddedProcessors/A3700-utils-marvell/archive/$UTILS_FILENAME
+    cp $UTILS_FILENAME $FQ_UTILS_FILENAME
+fi
+unzip $UTILS_FILENAME
+rm $UTILS_FILENAME
 sync
 wget --content-disposition http://wiki.espressobin.net/tiki-download_file.php?fileId=152
 wget --content-disposition http://wiki.espressobin.net/tiki-download_file.php?fileId=151
