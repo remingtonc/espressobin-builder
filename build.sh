177caa69669f9c50653dfaca39a7db89d9f34ccc
Remington Campbell <remcampb@cisco.com>
2018-08-24 01:20:57 -0700

    Enable MacOS (Darwin)

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+function run_darwin {
+    # https://source.android.com/setup/build/initializing#setting-up-a-mac-os-x-build-environment
+    hdiutil create -type SPARSE -fs 'Case-sensitive Journaled HFS+' -size 40g ./espressobin_build.dmg
+    hdiutil attach ./espressobin_build.dmg -mountpoint ./build/ \
+    || hdiutil attach ./espressobin_build.dmg.sparseimage -mountpoint ./build/
+    run_build
+    hdiutil detach ./build/
+    rm ./espressobin_build.dmg \
+    || rm ./espressobin_build.dmg.sparseimage
+}
+
+function run_build {
+    docker run --rm -it \
+        --name espressobin-build \
+        --mount type=bind,source="$(pwd)"/bin/,target=/data/ \
+        --mount type=bind,source="$(pwd)"/build/,target=/build/ \
+        --mount type=bind,source="$(pwd)"/cache/,target=/cache/ \
+        espressobin/build:latest
+}
+
 while true; do
     read -p "Reset build folders? [Y/N]: " yn
     case $yn in
@@ -8,9 +28,8 @@ while true; do
     esac
 done
 docker build -t espressobin/build .
-docker run -it \
-    --name espressobin-build \
-    --mount type=bind,source="$(pwd)"/bin/,target=/data/ \
-    --mount type=bind,source="$(pwd)"/build/,target=/build/ \
-    --mount type=bind,source="$(pwd)"/cache/,target=/cache/ \
-    espressobin/build:latest
+UNAME="$(uname -s)"
+case "${UNAME}" in
+    Linux*)     run_build;;
+    Darwin*)    run_darwin;;
+esac
