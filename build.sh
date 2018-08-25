4af453189c95d13557c5ed5022002d45dd46b199
Remington Campbell <remcampb@cisco.com>
2018-08-24 21:51:22 -0700

    Attempt using APFS volumes, untested

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 function run_darwin {
     # https://source.android.com/setup/build/initializing#setting-up-a-mac-os-x-build-environment
-    if [[ ! -f espressobin_build.dmg || ! -f espressobin_build.dmg.sparseimage ]]; then
-        hdiutil create -type SPARSE -fs 'Case-sensitive Journaled HFS+' -size 40g ./espressobin_build.dmg
-    fi
-    hdiutil attach ./espressobin_build.dmg.sparseimage -mountpoint ./build/ \
-    || hdiutil attach ./espressobin_build.dmg -mountpoint ./build/
+    # APFS volumes are wayyyyy faster than sparse disks
+    # https://www.dssw.co.uk/reference/diskutil.html
+    APFS_CONTAINER=$(diskutil apfs list | grep 'APFS Container Reference' | head -n 1 | awk '{print $4}')
+    APFS_DISK=$(sudo diskutil apfs addVolume $APFS_CONTAINER 'Case-sensitive APFS' espressobin_build -reserve 25g -quota 40g -mountpoint $(pwd)/build/ | grep 'New APFS disk' | awk '{print $4}')
     run_build
-    hdiutil detach ./build/
+    sudo diskutil apfs deleteVolume $APFS_DISK
 }
 
 function run_build {
@@ -25,7 +24,7 @@ function clean_build {
             rm -rf build/*
             ;;
         Darwin*)
-            rm espressobin_build.dmg*
+            printf "Cleaned implicitly.\n"
             ;;
         * )
             printf "Uncertain how to clean build!\n"
